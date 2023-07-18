507a0eb9eb3714287acb46227fa384ac1cf4ee9a
jaanonim <mat8mro@gmail.com>
2023-07-18 21:00:22 +0200

    Scene managment in work

---
+++ Assets/Scripts/EventManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//! THE SINGLETON 
+// Do not make more singletons
+// The one to rule them all, the one to find them, the one to bring them all and in the darkness bind them :-)
+
+public class EventManager : MonoBehaviour
+{
+    public static EventManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+            return;
+        }
+        Instance = this;
+    }
+
+
+
+}
---
+++ Assets/Scripts/SceneManger.cs
@@ -0,0 +1,159 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManger : MonoBehaviour
+{
+    public ScenesPreset neverUnload;
+    public float progress = 0;
+    public bool isLoading = false;
+
+    private struct SceneData
+    {
+        public int buildIndex;
+        public bool isLoaded;
+        public bool toLoad;
+        public bool toUnload;
+
+        public SceneData(int buildIndex, bool isLoaded, bool toLoad, bool toUnload)
+        {
+            this.buildIndex = buildIndex;
+            this.isLoaded = isLoaded;
+            this.toLoad = toLoad;
+            this.toUnload = toUnload;
+        }
+    }
+
+    private class SceneLoader
+    {
+
+        int activeScene;
+        List<SceneData> scenes;
+
+        public SceneLoader()
+        {
+            scenes = new List<SceneData>();
+            activeScene = SceneManager.GetActiveScene().buildIndex;
+        }
+
+        public void AddLoadedScens()
+        {
+            for (int i = 0; i < SceneManager.loadedSceneCount; i++)
+            {
+                int idx = SceneManager.GetSceneAt(i).buildIndex;
+                scenes.Add(new SceneData(idx, true, false, true));
+
+            }
+        }
+
+        public void AddScenesFromPreset(ScenesPreset preset)
+        {
+            List<int> toLoad = new List<int>();
+            foreach (SceneSetup scene in preset.Scenes)
+            {
+                int idx = SceneUtility.GetBuildIndexByScenePath(scene.path);
+                SceneData data = new SceneData(-1, false, false, false);
+                foreach (var item in scenes)
+                {
+                    if (idx == item.buildIndex)
+                    {
+                        data = item;
+                        break;
+                    }
+                }
+
+                if (data.buildIndex == -1)
+                {
+                    data = new SceneData(idx, false, true, false);
+                    scenes.Add(data);
+                }
+                data.toLoad = true;
+                if (scene.isActive) activeScene = idx;
+            }
+        }
+
+        public void DontUnloadFromPreset(ScenesPreset preset)
+        {
+            foreach (SceneSetup scene in preset.Scenes)
+            {
+                int idx = SceneUtility.GetBuildIndexByScenePath(scene.path);
+                SceneData data = new SceneData(-1, false, false, false);
+                foreach (var item in scenes)
+                {
+                    if (idx == item.buildIndex)
+                    {
+                        data = item;
+                        break;
+                    }
+                }
+                data.toUnload = false;
+            }
+        }
+
+        public List<SceneData> getScenes()
+        {
+            return scenes;
+        }
+    }
+
+    public void LoadFromPreset(ScenesPreset preset)
+    {
+        SceneLoader sceneLoader = new SceneLoader();
+
+        sceneLoader.AddLoadedScens();
+        sceneLoader.AddScenesFromPreset(preset);
+        sceneLoader.DontUnloadFromPreset(neverUnload);
+
+        StartCoroutine(LoadScenes(sceneLoader));
+    }
+
+    IEnumerator LoadScenes(SceneLoader sceneLoader)
+    {
+        isLoading = true;
+        progress = 0;
+
+        List<SceneData> scenes = sceneLoader.getScenes();
+        List<AsyncOperation> tasks = new List<AsyncOperation>();
+
+        foreach (SceneData scene in scenes)
+        {
+            if (scene.isLoaded && scene.toUnload)
+            {
+                tasks.Add(SceneManager.LoadSceneAsync(scene.buildIndex, LoadSceneMode.Additive));
+            }
+            else if (!scene.isLoaded && scene.toLoad)
+            {
+                tasks.Add(SceneManager.UnloadSceneAsync(scene.buildIndex));
+            }
+        }
+
+        while (true)
+        {
+            bool allDone = true;
+            float localProgress = 0f;
+            foreach (AsyncOperation task in tasks)
+            {
+                if (!task.isDone)
+                {
+                    allDone = false;
+                }
+                localProgress += task.progress;
+            }
+
+            if (allDone)
+            {
+                break;
+            }
+
+            progress = localProgress / tasks.Count;
+
+            yield return null;
+        }
+
+        isLoading = false;
+    }
+}
+
+
